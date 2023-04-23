fb0a732dc24c0369e2774ba3ccb1f0a6fc801432
Thennavan-Hex <thensthennavan3@gmail.com>
2023-04-23 21:24:40 +0530

    update

--- register.php
+++ register.php
@@ -1,14 +1,10 @@
 <?php
-# Include connection
 require_once "./config.php";
 
-# Define variables and initialize with empty values
 $username_err = $email_err = $password_err = "";
 $username = $email = $password = "";
 
-# Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  # Validate username
   if (empty(trim($_POST["username"]))) {
     $username_err = "Please enter a username.";
   } else {
@@ -16,22 +12,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!ctype_alnum(str_replace(array("@", "-", "_"), "", $username))) {
       $username_err = "Username can only contain letters, numbers and symbols like '@', '_', or '-'.";
     } else {
-      # Prepare a select statement
       $sql = "SELECT id FROM users WHERE username = ?";
 
       if ($stmt = mysqli_prepare($link, $sql)) {
-        # Bind variables to the statement as parameters
         mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-        # Set parameters
         $param_username = $username;
 
-        # Execute the prepared statement 
         if (mysqli_stmt_execute($stmt)) {
-          # Store result
           mysqli_stmt_store_result($stmt);
 
-          # Check if username is already registered
           if (mysqli_stmt_num_rows($stmt) == 1) {
             $username_err = "This username is already registered.";
           }
@@ -39,13 +29,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           echo "<script>" . "alert('Oops! Something went wrong. Please try again later.')" . "</script>";
         }
 
-        # Close statement 
         mysqli_stmt_close($stmt);
       }
     }
   }
 
-  # Validate email 
   if (empty(trim($_POST["email"]))) {
     $email_err = "Please enter an email address";
   } else {
@@ -53,22 +41,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $email_err = "Please enter a valid email address.";
     } else {
-      # Prepare a select statement
       $sql = "SELECT id FROM users WHERE email = ?";
 
       if ($stmt = mysqli_prepare($link, $sql)) {
-        # Bind variables to the statement as parameters
         mysqli_stmt_bind_param($stmt, "s", $param_email);
 
-        # Set parameters
         $param_email = $email;
 
-        # Execute the prepared statement 
         if (mysqli_stmt_execute($stmt)) {
-          # Store result
           mysqli_stmt_store_result($stmt);
 
-          # Check if email is already registered
           if (mysqli_stmt_num_rows($stmt) == 1) {
             $email_err = "This email is already registered.";
           }
@@ -76,13 +58,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
         }
 
-        # Close statement
         mysqli_stmt_close($stmt);
       }
     }
   }
 
-  # Validate password
   if (empty(trim($_POST["password"]))) {
     $password_err = "Please enter a password.";
   } else {
@@ -92,21 +72,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
 
-  # Check input errors before inserting data into database
   if (empty($username_err) && empty($email_err) && empty($password_err)) {
-    # Prepare an insert statement
     $sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
 
     if ($stmt = mysqli_prepare($link, $sql)) {
-      # Bind varibales to the prepared statement as parameters
       mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email, $param_password);
 
-      # Set parameters
       $param_username = $username;
       $param_email = $email;
       $param_password = password_hash($password, PASSWORD_DEFAULT);
 
-      # Execute the prepared statement
       if (mysqli_stmt_execute($stmt)) {
         echo "<script>" . "alert('Registeration completed successfully. Login to continue.');" . "</script>";
         echo "<script>" . "window.location.href='./login.php';" . "</script>";
@@ -115,12 +90,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
       }
 
-      # Close statement
       mysqli_stmt_close($stmt);
     }
   }
 
-  # Close connection
   mysqli_close($link);
 }
 ?>
@@ -146,7 +119,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="form-wrap border rounded p-4">
           <h1>Sign up</h1>
           <p>Please fill this form to register</p>
-          <!-- form starts here -->
           <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" novalidate>
             <div class="mb-3">
               <label for="username" class="form-label">Username</label>
@@ -172,7 +144,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <p class="mb-0">Already have an account ? <a href="./login.php">Log In</a></p>
           </form>
-          <!-- form ends here -->
         </div>
       </div>
     </div>
